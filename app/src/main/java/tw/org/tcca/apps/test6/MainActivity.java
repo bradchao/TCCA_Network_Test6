f8bb7a2e85604a19db98ab0efabf0d0e790ba65c
bradchao <brad@brad.tw>
2020-10-17 15:47:14 +0800

    3

--- app/src/main/java/tw/org/tcca/apps/test6/MainActivity.java
+++ app/src/main/java/tw/org/tcca/apps/test6/MainActivity.java
@@ -3,7 +3,9 @@ package tw.org.tcca.apps.test6;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
@@ -18,6 +20,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initWebView(){
-        webView.loadUrl("http://www.tcca.org.tw");
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+
+        //webView.loadUrl("https://www.iii.org.tw/");
+        webView.loadUrl("file:///android_asset/brad.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()){
+            webView.goBack();
+        }else {
+            super.onBackPressed();
+        }
     }
 }
