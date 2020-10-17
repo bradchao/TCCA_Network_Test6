fdd9d812c2a892a20b1a7e204dc2c5de5be0e900
bradchao <brad@brad.tw>
2020-10-17 15:17:25 +0800

    2

--- app/src/main/java/tw/org/tcca/apps/test6/MainActivity.java
+++ app/src/main/java/tw/org/tcca/apps/test6/MainActivity.java
@@ -3,12 +3,21 @@ package tw.org.tcca.apps.test6;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        initWebView();
+    }
+
+    private void initWebView(){
+        webView.loadUrl("http://www.tcca.org.tw");
     }
 }
